9dc79a659751754537b81e4144b56673f85535c3
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-04 10:35:38 +0900

    fix: 타입 수정

--- BE/src/types/request.types.ts
+++ BE/src/types/request.types.ts
@@ -1,6 +1,10 @@
+export type notionUrl = string;
+
+export type options = {
+  format?: "A4" | "Letter";
+};
+
 export interface ConvertRequest {
-  notionUrl: string;
-  options?: {
-    format?: "A4" | "Letter";
-  };
+  notionUrl: notionUrl;
+  options?: options;
 }
