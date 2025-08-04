aa6ba0eb34ed3110b1f4c37edfc237b8966ed19f
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-04 10:23:09 +0900

    feat: url 확인 함수 분리

---
+++ BE/src/util.ts
@@ -0,0 +1,11 @@
+const isValidNotionUrl = (url: string) => {
+  try {
+    const urlObj = new URL(url);
+    return (
+      urlObj.hostname.includes("notion.so") ||
+      urlObj.hostname.includes("notion.site")
+    );
+  } catch {
+    return false;
+  }
+};
