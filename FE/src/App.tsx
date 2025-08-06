8f6515408a646e35a7bac40125fce19202156c7d
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-06 13:38:51 +0900

    fix: button 및 핸들 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import './styles/App.css';
 import { apiClient } from './api/apiClient';
 
 function App() {
-  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [notionUrl, setNotionUrl] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,20 +18,47 @@ function App() {
     fetchData();
   }, []);
 
-  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const link = e.target.value;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNotionUrl(e.target.value);
+  };
+
+  const handleClick = async () => {
+    if (!notionUrl) return;
 
     try {
-      const res = await apiClient.post('/api/convert');
+      const res = await apiClient.post(
+        '/api/convert',
+        {
+          data: { notionUrl },
+        },
+        { responseType: 'blob' }
+      );
+
+      if (res.data) {
+        const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
+        const url = window.URL.createObjectURL(pdfBlob);
+
+        // a 태그 만들어 다운로드 처리
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'result.pdf'; // 저장할 파일명
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        window.URL.revokeObjectURL(url);
+      }
+
+      console.log(res);
     } catch (error) {
-      console.error(error);
+      console.error(error.response.data);
     }
   };
 
   return (
     <>
       <div>
-        <input type="text" ref={inputRef} onChange={handleChange} />
+        <input type="text" onChange={handleChange} />
+        <button onClick={handleClick}>CONVERT</button>
       </div>
     </>
   );
