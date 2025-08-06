b522539a26dbd44ed46d5b83bc0dbaf65ffb6aea
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-06 11:03:33 +0900

    feat: get 요청 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,8 +1,22 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import './styles/App.css';
+import { apiClient } from './api/apiClient';
 
 function App() {
   const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await apiClient.get('/');
+        console.log(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
