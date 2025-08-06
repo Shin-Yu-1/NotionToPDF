8c88c2fd115b402e96064cf737ce4f0510fdcf81
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-06 09:11:36 +0900

    feat: api 설정 추가

---
+++ FE/src/api/apiClient.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export const apiClient = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  // withCredentials: true, // 쿠키 인증 등이 필요할 경우
+});
