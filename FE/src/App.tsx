d4e404f6e863a88a5a02d2b7911a85869d62b8b3
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-06 11:09:42 +0900

    feat: post 요청 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -10,18 +10,28 @@ function App() {
       try {
         const res = await apiClient.get('/');
         console.log(res.data);
-      } catch (err) {
-        console.error(err);
+      } catch (error) {
+        console.error(error);
       }
     };
 
     fetchData();
   }, []);
 
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const link = e.target.value;
+
+    try {
+      const res = await apiClient.post('/api/convert');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <div>
-        <input type="text" ref={inputRef} />
+        <input type="text" ref={inputRef} onChange={handleChange} />
       </div>
     </>
   );
